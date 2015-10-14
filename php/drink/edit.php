8cde083aecae6815ac355bbab4f64b4a8fd5ff5b
Jonathan Odgis <jonathanodgis@gmail.com>
2015-10-14 20:24:00 +0000

    Removed the remember me inputs.

--- php/drink/edit.php
+++ php/drink/edit.php
@@ -99,16 +99,6 @@ session_start();
                             </div>
                           </div>      
 
-                       
-                          <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-10">
-                              <div class="checkbox">
-                                <label>
-                                  <input type="checkbox"> Remember me
-                                </label>
-                              </div>
-                            </div>
-                          </div>    
                           <div class="form-group">
                             <div class="col-sm-offset-2 col-sm-10">
                               <button type="submit" class="btn btn-success" id="submit">Record</button>
